a369d047b68c0fae02b8b75847b495f35f2cf77d
github username <github email address>
2019-08-30 11:39:32 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -14,7 +14,6 @@ class Owner
   def initialize name
     @name = name
     @species = "human"
-    # human_species
     self.class.all << self
   end
 
@@ -39,50 +38,40 @@ class Owner
   end
 
   def buy_cat(cat_name)
-    owner = self
-    Cat.new(cat_name, owner)
+    Cat.new(cat_name, self)
   end
 
   def buy_dog(dog_name)
-    owner = self
-    Dog.new(dog_name, owner)
+    Dog.new(dog_name, self)
   end
 
   def walk_dogs
-    self.dogs.map{ |dog| dog.mood = "happy" }
+    dogs.map{ |dog| dog.mood = "happy" }
   end
 
   def feed_cats
-    self.cats.map{ |cat| cat.mood = "happy" }
+    cats.map{ |cat| cat.mood = "happy" }
   end
 
+  def all_pets
+    dogs + cats
+  end
+  
   def nervous_pets
-    self.cats.map { |cat| cat.mood = "nervous" }
-    self.dogs.map { |dog| dog.mood = "nervous" }
+    all_pets.map { |pet| pet.mood = "nervous" }
   end
 
   def no_owner
-    self.cats.map { |cat| cat.owner = nil }
-    self.dogs.map { |dog| dog.owner = nil }
+    all_pets.map { |pet| pet.owner = nil }
   end
   
   def sell_pets
-    self.nervous_pets
-    self.no_owner
+    nervous_pets
+    no_owner
   end
 
   def list_pets
     "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
-
-  private
-
-  attr_writer :species
-  
-  def human_species
-    self.species = "human"
-  end
-
-
 
 end
